87efaf10de85f422098a7e35485e9be2db28ffc0
JonathanP34 <90928826+JonathanP34@users.noreply.github.com>
2021-11-24 14:27:54 -0500

    Delete FriendlyMob.java

--- FriendlyMob.java
+++
@@ -1,7 +0,0 @@
-public abstract class FriendlyMob extends Mob {
-   
-   public FriendlyMob(int x, int y, int z, int hp, int height, int width, int length, String type, boolean friendly, String drop, int dropAmount) {
-      super(x, y, z, hp, height, width, length, type, friendly, drop, dropAmount);
-   } 
-   
-}
