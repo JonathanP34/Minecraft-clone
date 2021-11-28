c16b8889ec087e1ad2c01f5b2c542067d16cc5f6
JonathanP34 <90928826+JonathanP34@users.noreply.github.com>
2021-11-28 18:34:19 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,7 @@
 /**
- * Testing/making terrain
- */
-/**
- *
+ * @date 2021-11-27
  * @author Jonathan Peters
+ * Main class to test all of the methods
  */
 public class Main {
     public static void main(String[] args) {
@@ -11,18 +9,42 @@ public class Main {
         Item[] inventory = new Item[3];
         inventory = Item.generateInventory();
         
-        Character c1 = new Character(0, 0, 0, 100, 1, 1, 1, "Character", 10.0, 0.0, 0, 1.0, "Steve");
-        
+        //Creating new instances of the item, character and block class
+        Character c1 = new Character(0, 0, 0, 100, 2, 1, 1, "Character", 10.0, 0.0, 0, 1.0, "Steve");
+        Item item = new Item("Bucket", 1, 2, 2, 0, 100);
+        PlacedBlock block = new PlacedBlock("Stone", 0, 0, 0, 50, 2);
+
+        //Testing character related methods
+        c1.crouch();
+        c1.crouch();
+        System.out.println("");
+
+        //Testing item related methods
+        item.lowerDurability();
+        item.getDurability();
+        item.removeConsumable();
+        item.getNumItem();
+        System.out.println();
+
+        //Testing block related methods
+        System.out.println(block.getDurability());
+        block.setDurability(110);
+        System.out.println(block.getItemResist());
+        System.out.println();
+
+        //Testing mineing and inventory methods
         c1.mineBlock(1, 1, 0);
         c1.setInventoryMarker(1);
         System.out.println("");
-
         c1.printInventory();
         System.out.println("");
 
+        //Testing character methods again
         System.out.println(c1.toString());
         System.out.println("");
         c1.eat(inventory[c1.getInventoryMarker()].getHungerChange());
+
+        //Testing moving and world builder methods
         for (int i = 0; i < 20; i++) {
             c1.move(1, 3, 0);
             if (c1.getHunger() == 1) {
@@ -31,7 +53,9 @@ public class Main {
             }
         }
         
+        //Killing the character
         c1.die("Boredom");
             
     }
 }
+
