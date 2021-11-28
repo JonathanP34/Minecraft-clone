4c7bf631d2f7cb2698066da68a71088c55474dae
JonathanP34 <90928826+JonathanP34@users.noreply.github.com>
2021-11-28 18:33:59 -0500

    Update WorldBuilder.java

--- WorldBuilder.java
+++ WorldBuilder.java
@@ -1,39 +1,47 @@
 import java.util.*;
+/**
+* Class to generate the terrain every time the user moves
+* @author Jonathan Peters
+* @date 2021-11-27
+*/
 
 public class WorldBuilder {
-    int[] blockLocationX = new int[9];
-    int[] blockLocationY = new int[9];
     
-    public WorldBuilder(){
+    public WorldBuilder(){ //Allows you to make an instance of the class
     }
     
     //Methods to do something
-    //Need to make the terrain generate actual blocks no just strings
     public static String generateTerrain(Character c1) {
+        //Creating instances of blocks that can be used to generate terrain
         PlacedBlock[] blocks;
         PlacedBlock[] createdBlocks = new PlacedBlock[4]; 
         createdBlocks = PlacedBlock.makeBlocks();
         
-        
+        //Making a 3 by 3 grid for the world
         PlacedBlock[][] generatedBlocks = new PlacedBlock[3][3];
-        Random rn = new Random();
+        Random rn = new Random(); //Random number generator 
         int answer;
         
+        //For loop to get a random number and get a block based on that number
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 answer = rn.nextInt(4);
-                generatedBlocks[i][j] = createdBlocks[answer];
+                //Adding the random block to the grid
+                generatedBlocks[i][j] = createdBlocks[answer]; 
             }  
         }
         
         String world = "";
         
+        //Printing the users coordinates
         world += "Your coordinates: " + c1.getX() + " " + c1.getY() + " " + c1.getZ() + "(You are in the middle)\n";
+        
+        //for loop to make the actual output from the type of blocks that were randomly generated
         for (int i = 0; i < 3; i++) { //Could make a second for loop to make it make a larger terrain
             world += generatedBlocks[i][0].getType() + "|" + generatedBlocks[i][1].getType() + "|" + generatedBlocks[i][2].getType() + "\n";
             world += "----------------\n";
-        }
-        
+        } 
+
         return world;
     }
 
